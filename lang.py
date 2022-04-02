dc056d77b0796c5e6c604de7950d28cb3bf06734
dj21-vicky21 <djvicky141@gmail.com>
2022-04-02 12:40:48 +0530

    lang.py

---
+++ lang.py
@@ -0,0 +1,146 @@
+# {'af': 'afrikaans', 
+# 'sq': 'albanian',
+# 'am': 'amharic', 
+# 'ar': 'arabic', 
+# 'hy': 'armenian', 
+# 'az': 'azerbaijani', 
+# 'eu': 'basque', 
+# 'be': 'belarusian', 
+# 'bn': 'bengali', 
+# 'bs': 'bosnian', 
+# 'bg': 'bulgarian', 
+# 'ca': 'catalan', 
+# 'ceb': 'cebuano', 
+# 'ny': 'chichewa', 
+# 'zh-cn': 'chinese (simplified)', 
+# 'zh-tw': 'chinese (traditional)', 
+# 'co': 'corsican', 
+# 'hr': 'croatian', 
+# 'cs': 'czech', 
+# 'da': 'danish', 
+# 'nl': 'dutch', 
+# 'en': 'english', 
+# 'eo': 'esperanto', 
+# 'et': 'estonian', 
+# 'tl': 'filipino', 
+# 'fi': 'finnish', 
+# 'fr': 'french', 
+# 'fy': 'frisian', 
+# 'gl': 'galician', 
+# 'ka': 'georgian', 
+# 'de': 'german', 
+# 'el': 'greek', 
+# 'gu': 'gujarati', 
+# 'ht': 'haitian creole', 
+# 'ha': 'hausa', 
+# 'haw': 'hawaiian', 
+# 'iw': 'hebrew', 
+# 'hi': 'hindi', 
+# 'hmn': 'hmong', 
+# 'hu': 'hungarian', 
+# 'is': 'icelandic', 
+# 'ig': 'igbo', 
+# 'id': 'indonesian', 
+# 'ga': 'irish', 
+# 'it': 'italian', 
+# 'ja': 'japanese', 
+# 'jw': 'javanese', 
+# 'kn': 'kannada', 
+# 'kk': 'kazakh', 
+# 'km': 'khmer', 
+# 'ko': 'korean', 
+# 'ku': 'kurdish (kurmanji)', 
+# 'ky': 'kyrgyz', 
+# 'lo': 'lao', 
+# 'la': 'latin', 
+# 'lv': 'latvian', 
+# 'lt': 'lithuanian', 
+# 'lb': 'luxembourgish', 
+# 'mk': 'macedonian', 
+# 'mg': 'malagasy', 
+# 'ms': 'malay', 
+# 'ml': 'malayalam', 
+# 'mt': 'maltese', 
+# 'mi': 'maori', 
+# 'mr': 'marathi', 
+# 'mn': 'mongolian', 
+# 'my': 'myanmar (burmese)', 
+# 'ne': 'nepali', 
+# 'no': 'norwegian', 
+# 'ps': 'pashto', 
+# 'fa': 'persian', 
+# 'pl': 'polish', 
+# 'pt': 'portuguese', 
+# 'pa': 'punjabi', 
+# 'ro': 'romanian', 
+# 'ru': 'russian', 
+# 'sm': 'samoan', 
+# 'gd': 'scots gaelic', 
+# 'sr': 'serbian', 
+# 'st': 'sesotho', 
+# 'sn': 'shona', 
+# 'sd': 'sindhi', 
+# 'si': 'sinhala', 
+# 'sk': 'slovak', 
+# 'sl': 'slovenian', 
+# 'so': 'somali', 
+# 'es': 'spanish', 
+# 'su': 'sundanese', 
+# 'sw': 'swahili', 
+# 'sv': 'swedish', 
+# 'tg': 'tajik', 
+# 'ta': 'tamil', 
+# 'te': 'telugu', 
+# 'th': 'thai', 
+# 'tr': 'turkish', 
+# 'uk': 'ukrainian', 
+# 'ur': 'urdu', 
+# 'uz': 'uzbek', 
+# 'vi': 'vietnamese', 
+# 'cy': 'welsh', 
+# 'xh': 'xhosa', 
+# 'yi': 'yiddish', 
+# 'yo': 'yoruba', 
+# 'zu': 'zulu', 
+# 'fil': 'Filipino', 
+# 'he': 'Hebrew'}
+
+def lang():
+    dic = {'ta': 'tamil','en': 'english', 'te': 'telugu','hi': 'hindi','fr': 'french','es': 'spanish','af': 'afrikaans', 'sq': 'albanian', 'am': 'amharic', 'ar': 'arabic', 'hy': 'armenian', 'az': 'azerbaijani', 'eu': 'basque', 'be': 'belarusian', 'bn': 'bengali', 'bs': 'bosnian', 'bg': 'bulgarian', 'ca': 'catalan', 'ceb': 'cebuano', 'ny': 'chichewa', 'zh-cn': 'chinese (simplified)', 'zh-tw': 'chinese (traditional)', 'co': 'corsican', 'hr': 'croatian', 'cs': 'czech', 'da': 'danish', 'nl': 'dutch',  'eo': 'esperanto', 'et': 'estonian', 'tl': 'filipino', 'fi': 'finnish',  'fy': 'frisian', 'gl': 'galician', 'ka': 'georgian', 'de': 'german', 'el': 'greek', 'gu': 'gujarati', 'ht': 'haitian creole', 'ha': 'hausa', 'haw': 'hawaiian', 'iw': 'hebrew',  'hmn': 'hmong', 'hu': 'hungarian', 'is': 'icelandic', 'ig': 'igbo', 'id': 'indonesian', 'ga': 'irish', 'it': 'italian', 'ja': 'japanese', 'jw': 'javanese', 'kn': 'kannada', 'kk': 'kazakh', 'km': 'khmer', 'ko': 'korean', 'ku': 'kurdish (kurmanji)', 'ky': 'kyrgyz', 'lo': 'lao', 'la': 'latin', 'lv': 'latvian', 'lt': 'lithuanian', 'lb': 'luxembourgish', 'mk': 'macedonian', 'mg': 'malagasy', 'ms': 'malay', 'ml': 'malayalam', 'mt': 'maltese', 'mi': 'maori', 'mr': 'marathi', 'mn': 'mongolian', 'my': 'myanmar (burmese)', 'ne': 'nepali', 'no': 'norwegian', 'ps': 'pashto', 'fa': 'persian', 'pl': 'polish', 'pt': 'portuguese', 'pa': 'punjabi', 'ro': 'romanian', 'ru': 'russian', 'sm': 'samoan', 'gd': 'scots gaelic', 'sr': 'serbian', 'st': 'sesotho', 'sn': 'shona', 'sd': 'sindhi', 'si': 'sinhala', 'sk': 'slovak', 'sl': 'slovenian', 'so': 'somali',  'su': 'sundanese', 'sw': 'swahili', 'sv': 'swedish', 'tg': 'tajik',  'th': 'thai', 'tr': 'turkish', 'uk': 'ukrainian', 'ur': 'urdu', 'uz': 'uzbek', 'vi': 'vietnamese', 'cy': 'welsh', 'xh': 'xhosa', 'yi': 'yiddish', 'yo': 'yoruba', 'zu': 'zulu', 'fil': 'Filipino', 'he': 'Hebrew'}
+    count = 1
+    words = []
+    for key ,item in dic.items():
+        words.append(item)
+    for i in words:
+        print("Enter No {} : {}".format(count,(i[0].upper()+i[1:])))
+        count+=1
+    lists = []
+    for key,item in dic.items():
+        lists.append(key)
+    lists = [None]+lists
+    # num =int(input("Enter the option: "))
+    # return (lists[num])
+    checker = True
+    while checker:
+        num =int(input("Enter the option: "))
+        if num <= len(dic):
+            return lists[num]
+            checker =False
+        else:
+            print('\nWrong Number Entered!')
+
+        
+
+tamil = lang()
+
+
+print(tamil)
+
+
+
+
+
+
+
+
+    
